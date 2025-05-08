630ae784701af83dec26f1223a9b2f2bba6c5e46
AfdanAiman <afdanaiman06@gmail.com>
2025-05-08 10:52:14 +0700

    Implementasi fungsi traverse pada listEmpty

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -90,4 +90,22 @@ bool delNode(int nim)
 }
 
 void traverse()
+    {
+        if (listEmpty())
+        {
+        cout << "\nList Kosong\n";
+
+        }
+        else
+        {
+        cout << "\nData didalam list adalah:\n";
+        Node *currentNode = START;
+        while (currentNode != NULL)
+            {
+            cout << currentNode->noMhs << endl;
+            currentNode = currentNode->next;
+            }
+        cout << endl;
+        }
+    }
 };
