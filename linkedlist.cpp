d77d6a19d91b94ff2026375fa4b2a62ad1df4756
AfdanAiman <afdanaiman06@gmail.com>
2025-05-08 10:37:44 +0700

    menambahkan listEmpety ke dalam variable

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -4,40 +4,74 @@ using namespace std;
 
 class Node
 {
-public:
-    int noMhs;
-    Node *next;
+    public:
+        int noMhs;
+        Node *next;
 };
 
-class List
+class singleLinkedList
 {
     Node *START;
 
-public:
-    List()
-    {
+    public:
+       singleLinkedList()
+       {
         START = NULL;
-    }
-
+       }
     void addNode()
     {
         int nim;
-        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cout << "\nMasukan Nomor Mahasiswa: ";
         cin >> nim;
         
-        Node *nodeBaru = new Node;
-        nodeBaru->noMhs = nim;
+    Node *nodebaru = new Node;
+    nodebaru->noMhs = nim;
 
-        if (START == NULL || nim <= START->noMhs)
-        {
-            if((START != NULL) && (nim == START->noMhs))
-            {
-                cout << "\nDuplikasi noMhs tidak diijinkan\n";
-                return;
-            }
-            cout << "\nDuplikasi noMhs tidak diijinkan\n";
-            return;
-        }
-        nodeBaru->next = NULL;
+    if (START == NULL ||  nim == START->noMhs)
+    {
+        if ((START != NULL) && (nim ++== START->noMhs))
+    {
+         cout << "\nDuplikasi noMhs tidak diijinkan\n";
+         return;
     }
+    nodebaru->next = START;
+    START = nodebaru;
+    return;
+}
+
+Node *previous = START;
+Node *current = START;
+
+while ((current != NULL) && (nim >= current->noMhs))
+{
+  if (nim == current->noMhs)
+  {
+    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+    return;
+  }
+  previous = current;
+  current = current->next;
+}
+
+nodebaru->next = current;
+previous->next = nodebaru;
+    }
+
+bool listEmpty()
+{
+    return (START == NULL);
+}
+bool Search(int nim, Node **previous, Node **current)
+{
+    *previous = START;
+    *current = START;
+
+    while ((*current != NULL) && (nim != (*current)->noMhs))
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+
+    return (*current != NULL);
+}
 };
